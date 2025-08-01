11ef0b7a97c3667b94aa3cc5b3a99203797692e7
shohaib1996 <khanshohaibhossain@gmail.com>
2025-08-01 23:06:45 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
